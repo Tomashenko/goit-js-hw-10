bb882aeca2d15cf9553f756255adceab799d3039
Tomashenko <114504099+Tomashenko@users.noreply.github.com>
2023-03-28 01:45:47 +0300

    27/03/2023

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,5 +1,4 @@
 
-import Notiflix from 'notiflix';
 export default function fetchCountries(name) {
     const url = `https://restcountries.com/v3.1/name/${name}?fields=name,capital,flags,languages,population`;
 
--- src/index.js
+++ src/index.js
@@ -8,49 +8,66 @@ const DEBOUNCE_DELAY = 300;
 const searchInput = document.querySelector("input#search-box");
 const countryList = document.querySelector('.country-list')
 const countryInfo = document.querySelector('.country-info')
-searchInput.addEventListener("input", debounce(onSearch, DEBOUNCE_DELAY));
 
+searchInput.addEventListener("input", debounce(onSearch, DEBOUNCE_DELAY));
 
 function onSearch(e) {
   e.preventDefault();
   name = e.target.value.trim();
-  console.log(name);
-  if (name === '') {return};
+  
+  if (name === ''){return};
+ 
   fetchCountries(name)
   .then(renderCountryList)
   .catch(onFetchError)
-};
+  };
 
 function renderCountryList (countries) {
+
   if(countries.length === 1){
-      const cardOfCountry = countries.map(({name, flags, capital, languages, population }) => {
+    clearMarkupConteiner();
+
+    const cardOfCountry = countries.map(({name, flags, capital, languages, population }) => {
       return `
-      
       <div class="current-country"
        <h2><img src="${flags.png}" alt="flag" width="30" height="30">${name.official}</h2>
       </div>
-        <ul>
-        <li>Capital: ${capital}</li>
-        <li>Population: ${population}</li>
-        <li>Languages: ${languages}</li>
+        <ul class="list-info">
+        <li><span class="list-item">Capital: </span> ${capital}</li>
+        <li><span class="list-item">Population: </span>${population}</li>
+        <li><span class="list-item">Languages: </span>${Object.values(languages)}</li>
         </ul>`;}).join('');
-      countryInfo.innerHTML = cardOfCountry;
+
+    countryInfo.innerHTML = cardOfCountry;
+
 } else if (2 <= countries.length < 11){
-    const markup = countries.map(({name, flags}) => {
+  
+  const markup = countries.map(({name, flags}) => {
       return `
       <li class="list"> 
       <img src="${flags.png}" alt="flag" width="30" height="30">
       ${name.official}
       </li>`;}).join('');
-      countryList.innerHTML = markup;
-  };
+
+    countryList.innerHTML = markup;
+};
+
   if(countries.length > 10) {
+    clearMarkupConteiner();
   return  Notiflix.Notify.info("Too many matches found. Please enter a more specific name.");
    };
 }
-// 
+
 function onFetchError(error) {
   Notiflix.Notify.failure("Oops, there is no country with that name");
+}
+
+function clearMarkupConteiner() {
+  countryList.innerHTML = '';
+}
+
+function clearCardOfCountry() {
+  cardOfCountry.innerHTML = '';
 }
 
 
